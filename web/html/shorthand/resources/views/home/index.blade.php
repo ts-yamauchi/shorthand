13bd0ad105f93b0c94f3146d3ef181c2960148f7
Yamauchi Tsubasa <ts-yamauchi@respect-pal.jp>
2016-09-13 12:35:14 +0900

    FIRST COMMIT

---
+++ web/html/shorthand/resources/views/home/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Shorthand</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('css/common.css')}}">
+
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="title">
+                    Shorthand
+                </div>
+            </div>
+        </div>
+
+        <div class="links">
+            <a href="https://laravel.com/docs">Write</a>
+            <a href="https://laracasts.com">Upload</a>
+        </div>
+    </div>
+</div>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
